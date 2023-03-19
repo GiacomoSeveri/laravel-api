e1f571e6cb0ffe7bd226cfe0e90ab39eda2c60c4
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-03-19 20:06:46 +0100

    added show route

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $projects = Project::with('category', 'lenguages')->orderBy('updated_at', 'DESC')->get();
+
+        foreach ($projects as $project) {
+            if ($project->image) $project->image = url('storsge' . $project->image);
+        }
+
+        return response()->json($projects);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $project = Project::find($id);
+        if (!$project) return response(null, 404);
+
+        return response()->json($project);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
